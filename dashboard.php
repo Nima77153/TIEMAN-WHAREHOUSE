ec434c14e115d069bda7a0ef258630c9c4780d89
Nima77153 <n0317527@gmail.com>
2026-08-07 15:11:36 +0800

    Fix all sidebar link paths

--- dashboard.php
+++ dashboard.php
@@ -171,6 +171,7 @@ if ($overrides_query) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome CDN added below -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
